cff39b2ef5d48929ccbbf10dd223865e90d41506
BjornL1 <>
2023-09-20 10:44:07 +0000

    script file, add function loop for adding dropdown options,update function calculate to handle only cost per month, amortization and total cost, add function for sending email

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,42 @@
+
+function quantity(amount) {
+    var select = document.getElementById('quantiti');
+    for (var i = 0; i < amount; i++) {
+        select.options[select.options.length] = new Option(i + 1, i);
+    }
+}
+
+quantity(50);
+
+function drop() {
+    let test = document.getElementById('quantiti').selectedIndex + 1;
+    document.getElementById('out').value = test + 1;
+    let testcost = 10;
+    testcost = test * testcost;
+    document.getElementById('output').innerHTML = testcost;
+
+}
+
+
+
+
 function calculate() {
+    /* code for dropdown function*/
+
+
+    /*rest of code*/
+
+
     let loan = document.getElementById('totalcost').value;
     let interestrate = document.getElementById('interest-rate').value;
-    let amortization = document.getElementById('monthly-payoff').value;
-    let payofftime = document.getElementById('year-payoff').value;
+    let amortization = 0;
+    let payofftime = document.getElementById('quantiti').selectedIndex + 1;
 
-
+    interestrate = interestrate / 100;
     let initialloan = loan;
     let totalloan = loan * interestrate;
     let monthlycost = (loan * interestrate) / 12;
+    let loancopy = 0;
     let counter = 0;
 
     let restmonth = 0;
@@ -15,24 +44,43 @@ function calculate() {
     var resttimemonth = 0;
     payofftime = payofftime * 12;
     var resttimeplus = resttime + payofftime;
+    var interestcosttime = 0;
+    interestcosttime = loan / payofftime;
+    var interestreal = (interestrate * initialloan) / 12;
+    var interestcopy = 0;
+    var monthlycostfinal = 0;
+    var month = 0;
+    let monthlycostfix = initialloan / payofftime;
+    let totalcost = 0;
+    let loanextra = 0;
+    let monthtotal = 0;
     document.getElementById('output').style.color = "white";
 
     for (let i = 0; i < payofftime; i++) {
 
+
+
         counter = counter + 1;
         totalloan = loan;
         monthlycost = (loan * interestrate) / 12;
-
-        loan = loan - amortization;
+        month = monthlycost;
+        loan = loan - monthlycostfix;
         resttimemonth = (loan / amortization);
-
+        monthlycostfinal = monthlycost + month;
+        loanextra = (loan * interestrate) / 12;
+        interestcopy = interestcopy + loanextra;
     }
 
     if (loan <= 0) {
+
+        monthlycost = (initialloan * interestrate) / 12;
+        interestcopy = interestcopy + interestreal;
+        totalcost = Math.ceil(monthlycostfix) * payofftime + interestcopy;
+        monthtotal = totalcost / counter;
         resttimeplus = initialloan / amortization;
         resttimeplus = Math.ceil(resttimeplus);
         document.getElementById('output').style.color = "green";
-        document.getElementById('output').innerHTML = "PASS! The loan will be paid within the target time. The loan will be paid after" + resttimeplus + "months";
+        document.getElementById('output').innerHTML = "The amortiziation cost per month will be: " + Math.ceil(monthlycostfix) + "." + " The total cost of the loan will be " + Math.ceil(totalcost) + " The average cost per month will be: " + Math.ceil(monthtotal);
     }
     else {
         restmonth = (initialloan / counter) - amortization;
@@ -45,3 +93,35 @@ function calculate() {
 function clearform() {
     document.getElementById("inputfields").reset();
 }
+
+
+function SendMail() {
+    var params = {
+        from_name: document.getElementById("fullName").value,
+        email_id: document.getElementById("email_id").value,
+        message: document.getElementById('output').innerHTML,
+    };
+    emailjs.send("service_j95t7f7", "template_k65yyrh", params).then(function (res) {
+        alert("Success! " + res.status);
+
+    });
+}
+
+
+
+
+
+/*
+
+
+
+
+function
+  var ind = document.getElementById('quantiti').selectedIndex;
+  var opt = document.getElementsByTagName('option')[ind].value;
+  var sudo = document.getElementsByTagName('option')[ind].innerHTML = opt;*/
+
+
+
+
+
